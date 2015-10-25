a62e2501eebdbb188bed381b769b9bc4b846433a
scalakid <alaniit007@gmail.com>
2015-10-25 11:46:46 +1100

    Re-Formatting comments section..

--- GuessRandomNumber/src/GuessRandomNumber.scala
+++ GuessRandomNumber/src/GuessRandomNumber.scala
@@ -11,25 +11,25 @@ object GuessRandomNumber {
   }
 
 
-  def startGame {
-    //function where actual code resides
+  def startGame {                     //function where actual code resides
 
-    var a = 0 //to store number guessed by user  variable, since value is read from keyboard
-    var count = 1 //count no. of chances,decrease in each iteration
+
+    var a = 0                        //to store number guessed by user  variable, since value is read from keyboard
+    var count = 1                    //count no. of chances,decrease in each iteration
     val randNum = scala.util.Random.nextInt(10) //generate random number between 0 and 9
 
     println("You have got 5 attempts.\nGuess a number between 0 and 9 (both inc.) : ...")
 
     do {
       print("Attempt " + count + " : Enter the number : ") //read the guessed number
-      a = io.StdIn.readInt //read from keyboard/standard input
-      count += 1 //increase number of attempts by one
+      a = io.StdIn.readInt                                //read from keyboard/standard input
+      count += 1                                         //increase number of attempts by one
 
       if (a == randNum)
         println("Congrats!!...Your guess is correct..:D")
       else if (count == 5)
         println("Sorry.Number of attempts over !!..:)\n Correct Number = " + randNum)
 
-    } while (count != 5 && a != randNum) //continue loop if attempts left and not guessed correctly
+    } while (count != 5 && a != randNum)              //continue loop if attempts left and not guessed correctly
   }//end of functon startGame
 }
